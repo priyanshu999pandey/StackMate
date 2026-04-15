90f20abaf6e7cd32f7ae4c37ce2dbfe2b4c72215
priyanshu999pandey <priyanshu999pandey@gmail.com>
2026-04-15 17:41:35 +0530

    added auth middleware and signup feed getUserById UpdateUser login api

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcrypt";
 import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res) => {
   try {
@@ -38,7 +39,7 @@ export const signup = async (req, res) => {
 
     // checking that user already have account with same email or not
     const userExist = await User.findOne({
-      $or: [{ email: email }, { username: username }],
+      $or: [{ email: email }, { userName: userName }],
     });
     if (userExist) {
       return res.status(400).json({
@@ -70,6 +71,143 @@ export const signup = async (req, res) => {
         email: userCreated.email,
         phoneNo: userCreated.phoneNo,
         username: userCreated.username,
+      },
+    });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({
+      message: "Internal server Error",
+      success: false,
+    });
+  }
+};
+
+export const feed = async (req, res) => {
+  try {
+    const allUsers = await User.find({});
+
+    if (!allUsers) {
+      return res.status(400).json({
+        message: "Users Not Found",
+        success: false,
+      });
+    }
+
+    res.status(200).json({
+      message: "Users Found Successfully",
+      success: true,
+      allUsers,
+    });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({
+      message: "Internal server Error",
+      success: false,
+    });
+  }
+};
+
+export const getUserById = async (req, res) => {
+  try {
+    console.log(req.body);
+    const userId = req.userId;
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(400).json({
+        message: "User Not Found",
+        success: false,
+      });
+    }
+
+    res.status(200).json({
+      message: "user found successfully",
+      success: true,
+      user,
+    });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({
+      message: "Internal server Error",
+      success: false,
+    });
+  }
+};
+
+export const UpdateUser = async (req, res) => {
+  try {
+    console.log(req.body);
+    const userId = req.userId;
+    const {firstName, lastName ,email, phoneNo} = req.body;
+
+    const updatedUser = await User.findOneAndUpdate(
+      { _id: userId }, 
+      {
+        firstName,
+        lastName,
+        email,
+        phoneNo
+      },
+      { new: true }, 
+    );
+
+    if (!updatedUser) {
+      return res.status(400).json({
+        message: "User Not Found",
+        success: false,
+      });
+    }
+
+    res.status(200).json({
+      message: "User Updated Successfully",
+      success: true,
+      updatedUser,
+    });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({
+      message: "Internal server Error",
+      success: false,
+    });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { userName, email, password } = req.body;
+
+    const user = await User.findOne({
+      $or: [{ email }, { userName }],
+    });
+
+    if (!user) {
+      return res.status(401).json({
+        message: "User Not Found",
+        success: false,
+      });
+    }
+
+    const matchPassword = bcrypt.compare(password, user.password);
+
+    if (!matchPassword) {
+      return res.status(401).json({
+        message: "Invalid credential",
+        success: false,
+      });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    res.status(200).json({
+      message: "Login successful",
+      token,
+      user: {
+        id: user._id,
+        username: user.userName,
+        email: user.email,
       },
     });
   } catch (error) {
--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -1,9 +1,14 @@
 import {Router} from "express"
-import { signup } from "../controllers/user.controller.js";
+import { feed, getUserById, login, signup, UpdateUser } from "../controllers/user.controller.js";
+import { auth } from "../middlewares/auth.middleware.js";
 
 const userRouter = Router();
 
 userRouter.post("/auth/signup",signup);
+userRouter.post("/auth/login",login);
+userRouter.get("/feed",auth,feed);
+userRouter.get("/getUserById",auth,getUserById);
+userRouter.patch("/updateUser",auth,UpdateUser);
 
 export default userRouter;
 
